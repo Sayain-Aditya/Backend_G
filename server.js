d358b0e9abbac5c40c531701fd4ccd7f282f0bf7
Sayain-Aditya <as4316664@gmail.com>
2025-07-15 15:55:07 +0530

    sa

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Export the app for Vercel
+module.exports = app;
